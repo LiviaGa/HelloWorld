a82fb7c99aca1cc10b93ddd06e6cc52e51f72b05
liviaGa <liviagallafrio1@gmail.com>
2025-02-28 16:11:30 -0300

    feat: Adicionando campo texto

--- app/src/main/java/liviaGa/com/github/helloworld/MainActivity.kt
+++ app/src/main/java/liviaGa/com/github/helloworld/MainActivity.kt
@@ -11,12 +11,37 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import liviaGa.com.github.helloworld.ui.theme.HelloWorldTheme
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+
+        // Referências para os componentes do layout
+        val etName = findViewById<EditText>(R.id.etName)
+        val tvHelloMessage = findViewById<TextView>(R.id.tvHelloMessage)
+
+        // Listener para quando o usuário pressiona Enter (actionDone)
+        etName.setOnEditorActionListener { textView, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val name = textView.text.toString().trim()
+                // Verifica se o nome foi digitado
+                if (name.isNotEmpty()) {
+                    tvHelloMessage.text = "Hello World, $name!"
+                } else {
+                    tvHelloMessage.text = "Hello World!"
+                }
+                true // Indica que consumimos o evento
+            } else {
+                false
+            }
+        }
 
     }
 }
